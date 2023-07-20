e3e8393a1d6cff21edb69baa82d9a7bf33846a3a
[Ranjit534] <ranjit121220@gmail.com>
2023-07-21 02:29:05 +0530

    Logic change

--- src/Components/Typing/Typing.js
+++ src/Components/Typing/Typing.js
@@ -1,102 +1,166 @@
-import React, { useState, useEffect } from "react";
-import "./Typing.css";
+import React, { useState, useEffect, useRef } from 'react';
+import { generate } from 'random-words';
+import './Typing.css';
 
-const RANDOM_QUOTE_API_URL = "http://api.quotable.io/random";
-
-const Timer = () => {
-  const [time, setTime] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime((prevTime) => prevTime + 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return <div className="timer">{time}</div>;
-};
-
-const QuoteDisplay = ({ quote, input }) => {
-  return (
-    <div className="quote-display">
-      {quote.split("").map((character, index) => (
-        <span
-          key={index}
-          className={
-            input[index] === undefined
-              ? ""
-              : input[index] === character
-              ? "correct"
-              : "incorrect"
-          }
-        >
-          {character}
-        </span>
-      ))}
-    </div>
-  );
-};
-
-const QuoteInput = ({ quote, onInputChange }) => {
-  return (
-    <textarea
-      className="quote-input"
-      value={quote}
-      onChange={onInputChange}
-      autoFocus
-    ></textarea>
-  );
-};
+const NUMB_OF_WORDS = 100;
+const SECONDS = 30;
 
 const Typing = () => {
-  const [quote, setQuote] = useState("");
-  const [quoteInput, setQuoteInput] = useState("");
+  const [words, setWords] = useState([]);
+  const [countDown, setCountDown] = useState(SECONDS);
+  const [currInput, setCurrInput] = useState('');
+  const [currWordIndex, setCurrWordIndex] = useState(0);
+  const [currCharIndex, setCurrCharIndex] = useState(-1);
+  const [currChar, setCurrChar] = useState('');
+  const [correct, setCorrect] = useState(0);
+  const [incorrect, setIncorrect] = useState(0);
+  const [status, setStatus] = useState('waiting');
+  const textInput = useRef(null);
 
   useEffect(() => {
-    renderNewQuote();
+    setWords(generateWords());
   }, []);
 
-  const renderNewQuote = () => {
-    fetch(RANDOM_QUOTE_API_URL)
-      .then((response) => response.json())
-      .then((data) => {
-        setQuote(data.content);
-        setQuoteInput("");
-      });
-  };
-
-  const handleInputChange = (event) => {
-    const { value } = event.target;
-    setQuoteInput(value);
-    checkInput();
-  };
-
-  const checkInput = () => {
-    const arrayQuote = quote.split("");
-    const arrayValue = quoteInput.split("");
-
-    let correct = true;
-    arrayQuote.forEach((character, index) => {
-      if (character === arrayValue[index]) {
-        return;
-      } else {
-        correct = false;
-      }
-    });
-
-    if (correct) {
-      renderNewQuote();
+  useEffect(() => {
+    if (status === 'started') {
+      textInput.current.focus();
     }
-  };
+  }, [status]);
+
+  function generateWords() {
+    return generate(NUMB_OF_WORDS);
+  }
+
+  function start() {
+    if (status === 'finished') {
+      setWords(generateWords());
+      setCurrWordIndex(0);
+      setCorrect(0);
+      setIncorrect(0);
+      setCurrCharIndex(-1);
+      setCurrChar('');
+    }
+
+    if (status !== 'started') {
+      setStatus('started');
+      let interval = setInterval(() => {
+        setCountDown((prevCountdown) => {
+          if (prevCountdown === 0) {
+            clearInterval(interval);
+            setStatus('finished');
+            setCurrInput('');
+            return SECONDS;
+          } else {
+            return prevCountdown - 1;
+          }
+        });
+      }, 1000);
+    }
+  }
+
+  function handleKeyDown({ keyCode, key }) {
+    if (keyCode === 32) {
+      checkMatch();
+      setCurrInput('');
+      setCurrWordIndex((prevIndex) => prevIndex + 1);
+      setCurrCharIndex(-1);
+    } else if (keyCode === 8) {
+      setCurrCharIndex((prevIndex) => Math.max(prevIndex - 1, -1));
+      setCurrChar('');
+    } else {
+      setCurrCharIndex((prevIndex) => prevIndex + 1);
+      setCurrChar(key);
+    }
+  }
+
+  function checkMatch() {
+    const wordToCompare = words[currWordIndex];
+    const doesItMatch = wordToCompare === currInput.trim();
+    if (doesItMatch) {
+      setCorrect((prevCorrect) => prevCorrect + 1);
+    } else {
+      setIncorrect((prevIncorrect) => prevIncorrect + 1);
+    }
+  }
+
+  function getCharClass(wordIdx, charIdx, char) {
+    if (
+      wordIdx === currWordIndex &&
+      charIdx === currCharIndex &&
+      currChar &&
+      status !== 'finished'
+    ) {
+      return char === currChar ? 'has-background-success' : 'has-background-danger';
+    } else if (wordIdx === currWordIndex && currCharIndex >= words[currWordIndex].length) {
+      return 'has-background-danger';
+    } else {
+      return '';
+    }
+  }
 
   return (
-    <div>
-      <Timer />
-      <div className="container">
-        <QuoteDisplay quote={quote} input={quoteInput} />
-        <QuoteInput quote={quoteInput} onInputChange={handleInputChange} />
+    <div className="SpeedTypingGame">
+      <div className="section">
+        <div className="is-size-1 has-text-centered has-text-primary">
+          <h2>{countDown}</h2>
+        </div>
       </div>
+      <div className="control is-expanded section">
+        <input
+          ref={textInput}
+          disabled={status !== 'started'}
+          type="text"
+          className="input"
+          onKeyDown={handleKeyDown}
+          value={currInput}
+          onChange={(e) => setCurrInput(e.target.value)}
+        />
+      </div>
+      <div className="section">
+        <button className="button is-info is-fullwidth" onClick={start}>
+          Start
+        </button>
+      </div>
+      {status === 'started' && (
+        <div className="section">
+          <div className="card">
+            <div className="card-content">
+              <div className="content">
+                {words.map((word, i) => (
+                  <span key={i}>
+                    {word.split('').map((char, idx) => (
+                      <span className={getCharClass(i, idx, char)} key={idx}>
+                        {char}
+                      </span>
+                    ))}
+                    <span> </span>
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {status === 'finished' && (
+        <div className="section">
+          <div className="columns">
+            <div className="column has-text-centered">
+              <p className="is-size-5">Words per minute:</p>
+              <p className="has-text-primary is-size-1">{correct}</p>
+            </div>
+            <div className="column has-text-centered">
+              <p className="is-size-5">Accuracy:</p>
+              {correct !== 0 ? (
+                <p className="has-text-info is-size-1">
+                  {Math.round((correct / (correct + incorrect)) * 100)}%
+                </p>
+              ) : (
+                <p className="has-text-info is-size-1">0%</p>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
